c3bb90ac748a9b990c792307835ccd7a1c586e34
Brian <bbensch@gmail.com>
2016-07-12 00:04:34 -0700

    update mailers, confirm weird bug with booked_lessons array including a nil value

--- app/mailers/lesson_mailer.rb
+++ app/mailers/lesson_mailer.rb
@@ -16,9 +16,25 @@ class LessonMailer < ActionMailer::Base
     mail(to: 'notify@snowschoolers.com', bcc: @available_instructors, subject: 'New Snow Schoolers lesson request')
   end
 
+  def send_lesson_request_to_new_instructors(lesson, excluded_instructor=nil)
+    @lesson = lesson
+    available_instructors = (lesson.available_instructors - [excluded_instructor])
+    @available_instructors = []
+    available_instructors.each do |instructor|
+      @available_instructors << instructor.email
+    end
+    mail(to: 'notify@snowschoolers.com', bcc: @available_instructors, subject: 'A previous instructor canceled - can you help with this lesson request?')
+  end
+
+  # notification when instructor cancels
+  def send_cancellation_confirmation(lesson)
+    @lesson = lesson
+    mail(to: @lesson.instructor.email, cc:'notify@snowschoolers.com', subject: 'You have canceled your Snow Schoolers lesson')
+  end
+
   def send_lesson_confirmation(lesson)
     @lesson = lesson
-    mail(to: @lesson.requester.email, cc:'notify@snowschoolers.com', subject: 'Your Snow Schoolers lesson has been confirmed')
+    mail(to: @lesson.requester.email, cc:'notify@snowschoolers.com', bcc:@lesson.instructor.email, subject: 'Your Snow Schoolers lesson has been confirmed')
   end
 
   def send_lesson_update_notice_to_instructor(original_lesson, updated_lesson, changed_attributes)
@@ -28,9 +44,10 @@ class LessonMailer < ActionMailer::Base
     mail(to: @updated_lesson.instructor.email, cc:'notify@snowschoolers.com', subject: 'One of your Snow Schoolers lesson has been updated')
   end
 
+  # notification when client cancels
   def send_cancellation_email_to_instructor(lesson)
     @lesson = lesson
-    mail(to: @lesson.instructor.email, cc:'notify@snowschoolers.com', subject: 'One of your Snow Schoolers lessons has been canceled')
+    mail(to: @lesson.instructor.email, cc:'notify@snowschoolers.com', bcc: @lesson.requester.email, subject: 'One of your Snow Schoolers lessons has been canceled')
   end
 
   def inform_requester_of_instructor_cancellation(lesson, available_instructors)
